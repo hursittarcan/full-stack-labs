3804f90a6411f40f09a588baa001c4c4a4e8d6d4
HursitTarcan <HursitTarcan@gmail.com>
2022-09-29 22:30:16 +0200

    news-letter component added

--- java-frontend/exercises/exercise-01/chapter3-oefening1/src/app/app.module.ts
+++ java-frontend/exercises/exercise-01/chapter3-oefening1/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { NewsLetterComponent } from './components/news-letter/news-letter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent
+    LogoComponent,
+    NewsLetterComponent
   ],
   imports: [
     BrowserModule
