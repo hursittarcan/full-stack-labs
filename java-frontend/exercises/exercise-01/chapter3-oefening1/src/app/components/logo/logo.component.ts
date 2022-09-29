dc321f3c20a6b5873a2e37f2659b24ee4573578d
HursitTarcan <HursitTarcan@gmail.com>
2022-09-29 22:38:29 +0200

    logo component updated

--- java-frontend/exercises/exercise-01/chapter3-oefening1/src/app/components/logo/logo.component.ts
+++ java-frontend/exercises/exercise-01/chapter3-oefening1/src/app/components/logo/logo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-logo-component',
+  selector: 'app-logo',
   template: '<div><img src="assets/image.png" alt="logo" height="140px"></div>',
   styles: ['div { background: green; width: 100%; height: 140px;}']
 })
