aba1adcefcb33ba36bfc7d2336a438d7458ac951
HursitTarcan <HursitTarcan@gmail.com>
2022-10-04 16:38:29 +0200

    db context class added

---
+++ net-backend/lab-01/KWops/src/Services/HumanResources/HumanResources.Infrastructure/HumanResourcesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources.Infrastructure
+{
+    internal class HumanResourcesContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+        
