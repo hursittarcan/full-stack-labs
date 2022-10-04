4eb46b380914e54dde41f5621aabc74827455bc7
HursitTarcan <HursitTarcan@gmail.com>
2022-10-04 16:46:22 +0200

    employee config updated

---
+++ net-backend/lab-01/KWops/src/Services/HumanResources/HumanResources.Infrastructure/EmployeeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using HumanResources.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources.Infrastructure
+{
+    internal class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder
+                .HasKey(b => b.Number)
+                .HasMaxLength(11); 
+            throw new NotImplementedException();
+        }
+    }
+}
