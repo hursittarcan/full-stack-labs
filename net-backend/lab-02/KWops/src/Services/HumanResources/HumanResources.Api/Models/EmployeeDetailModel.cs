8c065eed5a5da73162428cbbf2a598d5a56e5f50
HursitTarcan <HursitTarcan@gmail.com>
2022-10-17 21:12:24 +0200

    public class EmployeeDetailModel

---
+++ net-backend/lab-02/KWops/src/Services/HumanResources/HumanResources.Api/Models/EmployeeDetailModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HumanResources.Api.Models
+{
+    public class EmployeeDetailModel
+    {
+        public string Number { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
