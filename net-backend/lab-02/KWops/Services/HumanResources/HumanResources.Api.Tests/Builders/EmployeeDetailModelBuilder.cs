2eb48284609f92a3c592ea5e7db380f59bcf4a19
HursitTarcan <HursitTarcan@gmail.com>
2022-10-17 22:08:34 +0200

    EmployeeDetailModelBuilder added

---
+++ net-backend/lab-02/KWops/Services/HumanResources/HumanResources.Api.Tests/Builders/EmployeeDetailModelBuilder.cs
@@ -0,0 +1,21 @@
+﻿using HumanResources.Api.Models;
+using System;
+using Test;
+
+namespace HumanResources.Api.Tests.Builders
+{
+    internal class EmployeeDetailModelBuilder : BuilderBase<EmployeeDetailModel>
+    {
+        public EmployeeDetailModelBuilder()
+        {
+            Item = new EmployeeDetailModel
+            {
+                Number = Random.NextString(),
+                FirstName = Random.NextString(),
+                LastName = Random.NextString(),
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddMonths(1)
+            };
+        }
+    }
+}
