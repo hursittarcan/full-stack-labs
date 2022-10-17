288acd3faee135c33df2675636a7f8e2eb544c04
HursitTarcan <HursitTarcan@gmail.com>
2022-10-17 19:27:26 +0200

    model folder + class added

---
+++ net-backend/lab-02/KWops/src/Services/HumanResources/HumanResources.Api/Models/EmployeeCreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HumanResources.Api.Models
+{
+    public class EmployeeCreateModel
+    {
+        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+    }
+}
--- net-backend/lab-02/KWops/src/Services/HumanResources/HumanResources.AppLogic/EmployeeService .cs
+++ net-backend/lab-02/KWops/src/Services/HumanResources/HumanResources.AppLogic/EmployeeService .cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HumanResources.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace HumanResources.AppLogic
 {
-    internal class EmployeeService
+    internal class EmployeeService : IEmployeeService
     {
+        private readonly IEmployeeFactory _employeeFactory;
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeFactory employeeFactory, IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _employeeFactory = employeeFactory; 
+        }
+
+        public async Task<IEmployee> HireNewAsync(string lastName, string firstName, DateTime startDate)
+        {
+            int sequence = await _employeeRepository.GetNumberOfStartersOnAsync(startDate);
+            var newEmployee = _employeeFactory.CreateNew(lastName, firstName, startDate, sequence); 
+             _employeeRepository.AddAsync(newEmployee);
+            return null; 
+        }
     }
 }
