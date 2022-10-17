574b012b5b1182b6f609098cb7f6cf60b1177ec1
HursitTarcan <HursitTarcan@gmail.com>
2022-10-17 18:18:11 +0200

    step 1 completed

---
+++ net-backend/lab-02/KWops/BuildingBlocks/Domain/ContractException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Domain
+{
+    /// <summary>
+    /// Thrown when a certain contract is not met.
+    /// Usually thrown by <see cref="Contracts"/> class.
+    /// </summary>
+    [Serializable]
+    public class ContractException : Exception
+    {
+        public ContractException()
+        {
+        }
+
+        public ContractException(string message)
+            : base(message)
+        {
+        }
+
+        public ContractException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected ContractException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
