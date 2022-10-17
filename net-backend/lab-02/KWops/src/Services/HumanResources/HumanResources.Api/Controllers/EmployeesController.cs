b253ea6a9f134adae90a4aa81003bc5521386e79
HursitTarcan <HursitTarcan@gmail.com>
2022-10-17 21:17:55 +0200

    employee controller updated

--- net-backend/lab-02/KWops/src/Services/HumanResources/HumanResources.Api/Controllers/EmployeesController.cs
+++ net-backend/lab-02/KWops/src/Services/HumanResources/HumanResources.Api/Controllers/EmployeesController.cs
@@ -1,35 +1,38 @@
-﻿using HumanResources.Api.Models;
+﻿using AutoMapper;
+using HumanResources.Api.Models;
 using HumanResources.AppLogic;
 using HumanResources.Domain;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HumanResources.Api.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeesController : ControllerBase
 {
-    [Route("/employees")]
-    [ApiController]
-    public class EmployeesController : Controller
+    private readonly IEmployeeService _employeeService;
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly IMapper _mapper;
+
+    public EmployeesController(IEmployeeService employeeService,
+        IEmployeeRepository employeeRepository,
+        IMapper mapper)
     {
-        private readonly IEmployeeRepository _employeeRepository;
-        private readonly IEmployeeService _employeeService;
+        _employeeService = employeeService;
+        _employeeRepository = employeeRepository;
+        _mapper = mapper;
+    }
 
-        public EmployeesController(IEmployeeRepository employeeRepository, IEmployeeService employeeService)
-        {
-            _employeeRepository = employeeRepository;
-            _employeeService = employeeService;
-        }
+    [HttpGet("{number}")]
+    public async Task<IActionResult> GetByNumber(string number)
+    {
+        IEmployee? employee = await _employeeRepository.GetByNumberAsync(number);
+        return employee == null ? NotFound() : Ok(_mapper.Map<EmployeeDetailModel>(employee));
+    }
 
-        [HttpGet("{number}")]
-        public async Task<IActionResult> GetByNumber(string number)
-        {
-            IEmployee? employee = await _employeeRepository.GetByNumberAsync(number);
-            return employee == null ? NotFound() : Ok(employee);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Add(EmployeeCreateModel model)
-        {
-            IEmployee hiredEmployee = await _employeeService.HireNewAsync(model.LastName, model.FirstName, model.StartDate);
-            return CreatedAtAction(nameof(GetByNumber), new { number = hiredEmployee.Number }, hiredEmployee);
-        }
+    [HttpPost]
+    public async Task<IActionResult> Add(EmployeeCreateModel model)
+    {
+        IEmployee hiredEmployee = await _employeeService.HireNewAsync(model.LastName, model.FirstName, model.StartDate);
+        var outputModel = _mapper.Map<EmployeeDetailModel>(hiredEmployee);
+        return CreatedAtAction(nameof(GetByNumber), new { number = outputModel.Number }, outputModel);
     }
 }
