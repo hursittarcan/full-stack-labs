974feb680250dda2be360a87d04068ce497d682b
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 17:14:31 +0200

    ITeamService added

---
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.AppLogic/ITeamService.cs
@@ -0,0 +1,9 @@
+﻿using DevOps.Domain;
+
+namespace DevOps.AppLogic
+{
+    public interface ITeamService
+    {
+        Task AssembleDevelopersAsyncFor(Team team, int requiredNumberOfDevelopers);
+    }
+}
