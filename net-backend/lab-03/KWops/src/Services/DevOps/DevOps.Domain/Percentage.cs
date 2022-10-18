1f90b238b0e0cfcbb3fafc27a374e235d4c200bf
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 15:26:14 +0200

    percentage.cs added

---
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.Domain/Percentage.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps.Domain
+{
+    public class Percentage : ValueObject<Percentage>
+    {
+        private readonly double _value;
+
+        public Percentage(double value)
+        {
+            _value = value;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return _value;
+        }
+    }
+}
