ee88aad31b3b0d9db0d2f7bea251366db640a28c
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 17:36:56 +0200

    logics tests pass

--- net-backend/lab-03/KWops/src/Services/DevOps/DevOps.AppLogic/TeamService.cs
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.AppLogic/TeamService.cs
@@ -9,13 +9,36 @@ namespace DevOps.AppLogic
 {
     internal class TeamService : ITeamService
     {
+        IDeveloperRepository _developerRepository;
+        Random _random; 
+
         public TeamService(IDeveloperRepository developerRepository)
         {
+            _developerRepository = developerRepository;
+            _random = new Random();
         }
 
-        public Task AssembleDevelopersAsyncFor(Team team, int requiredNumberOfDevelopers)
+        public async Task AssembleDevelopersAsyncFor(Team team, int requiredNumberOfDevelopers)
         {
-            throw new NotImplementedException();
+            var developers = await _developerRepository.FindDevelopersWithoutATeamAsync();
+            List<Developer> developersList = developers.Where(x => x.TeamId != team.Id).ToList();
+
+            if (developers.Count >= requiredNumberOfDevelopers)
+            {
+                while (team.Developers.Count < requiredNumberOfDevelopers)
+                {
+                    Developer developer = developersList[_random.Next(0, developersList.Count)];
+                    if (!team.Developers.Contains(developer)) team.Join(developer);
+                }
+            }
+            else
+            {
+                foreach (Developer developer in developersList)
+                {
+                    team.Join(developer);
+                }
+            }
+            await _developerRepository.CommitTrackedChangesAsync();
         }
     }
 }
