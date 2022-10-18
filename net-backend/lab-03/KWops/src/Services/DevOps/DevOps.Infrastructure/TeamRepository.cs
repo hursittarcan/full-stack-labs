3408f0d0ccbce34b664c5c458599b0c7e90031ca
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 18:51:58 +0200

    team repo implemented

---
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.Infrastructure/TeamRepository.cs
@@ -0,0 +1,33 @@
+﻿using DevOps.AppLogic;
+using DevOps.Domain;
+using DevOps.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResources.Infrastructure
+{
+    internal class TeamRepository : ITeamRepository
+    {
+        private readonly DevOpsContext _devOpsContext;
+
+        public TeamRepository(DevOpsContext devOpsContext)
+        {
+            _devOpsContext = devOpsContext;
+        }
+
+        public async Task<IReadOnlyList<Team>> GetAllAsync()
+        {
+            return await _devOpsContext.Teams.Include(team => team.Developers.ToList()).ToListAsync();
+        }
+
+        public async Task<Team?> GetByIdAsync(Guid teamId)
+        {
+            var team = await _devOpsContext.Teams.Include(team => team.Developers.ToList()).ToListAsync();
+            return team.Where(team => team.Id == teamId).FirstOrDefault();
+        }
+    }
+}
