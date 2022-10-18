88a37ee749e13c493c7939bf2a54d8883aac14ae
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 16:11:51 +0200

    Team Entity Added

---
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.Domain/Team.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps.Domain
+{
+    public class Team : Entity
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+        private readonly List<Developer> _developers;
+        public IReadOnlyList<Developer> Developers => _developers;
+
+        private Team(Guid id, string name)
+        {
+            _developers = new List<Developer>();
+        }
+
+        public static Team CreateNew(string name)
+        {
+            return null;
+        }
+
+        public void Join(Developer developer)
+        {
+
+        }
+
+        protected override IEnumerable<object> GetIdComponents()
+        {
+            yield return Id;
+        }
+    }
+}
