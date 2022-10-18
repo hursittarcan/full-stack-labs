607fc2068bff02ae12cadb0fc9661a80297b1755
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 17:06:47 +0200

    test builders added

---
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.Domain.Tests/Builders/DeveloperBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test;
+
+namespace DevOps.Domain.Tests.Builders
+{
+    public class DeveloperBuilder : BuilderBase<Developer>
+    {
+        public DeveloperBuilder()
+        {
+            Item = Developer.CreateNew(Random.NextString(), Random.NextString(), Random.NextString(),
+                Random.NextDouble());
+        }
+
+        public DeveloperBuilder WithoutTeam()
+        {
+            SetProperty(e => e.TeamId, null);
+            return this;
+        }
+
+        public DeveloperBuilder WithTeam(Team team)
+        {
+            SetProperty(e => e.TeamId, team.Id);
+            return this;
+        }
+    }
+}
---
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.Domain.Tests/Builders/TeamBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace DevOps.Domain.Tests.Builders
+{
+    public class TeamBuilder : BuilderBase<Team>
+    {
+        private readonly List<Developer> _developers;
+
+        public TeamBuilder()
+        {
+            Item = Team.CreateNew(Random.NextString());
+
+            FieldInfo? developersBackingField = typeof(Team).GetField("_developers", BindingFlags.NonPublic | BindingFlags.Instance);
+            _developers = developersBackingField?.GetValue(Item) as List<Developer> ??
+                          throw new Exception("Cannot find _developers backing field of Team");
+        }
+
+        public TeamBuilder WithDeveloper(Developer developer)
+        {
+            _developers.Add(developer);
+            return this;
+        }
+    }
+}
