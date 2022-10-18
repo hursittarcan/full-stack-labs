413ac953a6412fbdec1d1a3b1995be275b8de6b0
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 17:41:10 +0200

    DevOpsContext added

---
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.Infrastructure/DevOpsContext.cs
@@ -0,0 +1,19 @@
+﻿using DevOps.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps.Infrastructure
+{
+    internal class DevOpsContext : DbContext
+    {
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+        public DevOpsContext(DbContextOptions options)
+            : base(options) { }
+    }
+}
