ace8d826906957e5c97112013288294d9cc74690
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 19:08:30 +0200

    program.cs updated

--- net-backend/lab-03/KWops/src/Services/DevOps/DevOps.Api/Program.cs
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.Api/Program.cs
@@ -1,6 +1,22 @@
+using DevOps.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+ConfigurationManager configuration = builder.Configuration;
+builder.Services.AddDbContext<DevOpsContext>(options =>
+{
+    string connectionString = configuration["ConnectionString"];
+    options.UseSqlServer(connectionString, sqlOptions =>
+    {
+        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+    });
+#if DEBUG
+    options.UseLoggerFactory(LoggerFactory.Create(loggingBuilder => loggingBuilder.AddDebug()));
+    options.EnableSensitiveDataLogging();
+#endif
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
