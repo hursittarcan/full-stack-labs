4490343c26c8b6571c7ced2b48be46e8c647ff79
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 17:16:10 +0200

    TeamService implemented

---
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.AppLogic/TeamService.cs
@@ -0,0 +1,21 @@
+﻿using DevOps.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps.AppLogic
+{
+    internal class TeamService : ITeamService
+    {
+        public TeamService(IDeveloperRepository developerRepository)
+        {
+        }
+
+        public Task AssembleDevelopersAsyncFor(Team team, int requiredNumberOfDevelopers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
