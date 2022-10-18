c8f78cc565881c53fa556ecd1fa22a1affc90a3b
HursitTarcan <HursitTarcan@gmail.com>
2022-10-18 19:47:29 +0200

    teamsController added

---
+++ net-backend/lab-03/KWops/src/Services/DevOps/DevOps.Api/Controllers/TeamsController.cs
@@ -0,0 +1,24 @@
+﻿using DevOps.Api.Models;
+using DevOps.AppLogic;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class TeamsController : ControllerBase
+{
+    public TeamsController(ITeamService teamService, ITeamRepository teamRepository, IMapper mapper)
+    {
+    }
+
+    [HttpGet]
+    public Task<IActionResult> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("{id}/assemble")]
+    public Task<IActionResult> AssembleTeam(Guid id, TeamAssembleInputModel model)
+    {
+        throw new NotImplementedException();
+    }
+}
