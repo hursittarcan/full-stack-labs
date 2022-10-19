83bbf3b30f7a1f1b5d36b394b8d139683f0053e6
HursitTarcan <HursitTarcan@gmail.com>
2022-10-19 12:49:34 +0200

    employee hired integration event builder added

---
+++ net-backend/lab-04/KWops/src/Services/DevOps/DevOps.AppLogic.Tests/Builders/EmployeeHiredIntegrationEventBuilder.cs
@@ -0,0 +1,19 @@
+﻿using DevOps.AppLogic.Events;
+using System;
+using Test;
+
+namespace DevOps.AppLogic.Tests.Builders
+{
+    internal class EmployeeHiredIntegrationEventBuilder : BuilderBase<EmployeeHiredIntegrationEvent>
+    {
+        public EmployeeHiredIntegrationEventBuilder()
+        {
+            Item = new EmployeeHiredIntegrationEvent
+            {
+                Number = Random.NextString(),
+                FirstName = Random.NextString(),
+                LastName = Random.NextString()
+            };
+        }
+    }
+}
