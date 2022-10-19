7e0d26e1c926538813729967dbf42d423481ae8d
HursitTarcan <HursitTarcan@gmail.com>
2022-10-19 12:28:51 +0200

    IntegrationEventHandler interface added

---
+++ net-backend/lab-04/KWops/src/BuildingBlocks/AppLogic/Events/IIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogic.Events
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent>
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+}
