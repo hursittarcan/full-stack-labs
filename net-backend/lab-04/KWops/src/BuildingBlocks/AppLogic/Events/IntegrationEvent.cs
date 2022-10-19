cb52b777caa3286ac731419d77c1ba2529670f72
HursitTarcan <HursitTarcan@gmail.com>
2022-10-19 12:22:56 +0200

    IntegrationEvent added

---
+++ net-backend/lab-04/KWops/src/BuildingBlocks/AppLogic/Events/IntegrationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AppLogic.Events
+{
+    public class IntegrationEvent
+    {
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        [JsonConstructor]
+        public IntegrationEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreationDate = createDate;
+        }
+
+        public Guid Id { get; private set; }
+
+        public DateTime CreationDate { get; private set; }
+    }
+}
