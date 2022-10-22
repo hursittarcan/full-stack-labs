da3e36d752ce9d92c13d4c77500d629fef4f507f
HursitTarcan <HursitTarcan@gmail.com>
2022-10-22 14:50:28 +0200

    two way binding part 1 complete

--- java-frontend/lab-03/src/app/components/add-confession/add-confession.component.ts
+++ java-frontend/lab-03/src/app/components/add-confession/add-confession.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {Confession} from "../../models/confession.model";
 
 @Component({
   selector: 'app-add-confession',
   templateUrl: './add-confession.component.html',
   styleUrls: ['./add-confession.component.css']
 })
-export class AddConfessionComponent implements OnInit {
 
+export class AddConfessionComponent implements OnInit {
+  confession!: Confession;
   constructor() { }
 
   ngOnInit(): void {
+    this.confession = new Confession('', '', '');
   }
 
 }
+
