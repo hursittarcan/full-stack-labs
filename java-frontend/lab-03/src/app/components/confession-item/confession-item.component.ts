4547f5012b160d10ae94876d26f906084f09df4a
HursitTarcan <HursitTarcan@gmail.com>
2022-10-22 14:47:06 +0200

    one way binding done

--- java-frontend/lab-03/src/app/components/confession-item/confession-item.component.ts
+++ java-frontend/lab-03/src/app/components/confession-item/confession-item.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {Confession} from "../../models/confession.model";
 
 @Component({
   selector: 'app-confession-item',
   templateUrl: './confession-item.component.html',
   styleUrls: ['./confession-item.component.css']
 })
-export class ConfessionItemComponent implements OnInit {
 
+export class ConfessionItemComponent implements OnInit {
+  confession!: Confession;
   constructor() { }
 
   ngOnInit(): void {
+    this.confession = new Confession('Mondays are the worst','PXL-Digital','anonymous',false);
   }
 
 }
