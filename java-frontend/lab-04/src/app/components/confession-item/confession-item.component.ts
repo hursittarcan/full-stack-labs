be62a28d9877c1073a84be6c31535151c0dc2265
HursitTarcan <HursitTarcan@gmail.com>
2022-10-24 12:00:16 +0200

    revision part completed

--- java-frontend/lab-04/src/app/components/confession-item/confession-item.component.ts
+++ java-frontend/lab-04/src/app/components/confession-item/confession-item.component.ts
@@ -1,18 +1,26 @@
+// @ts-ignore
 import { Component, Input, OnInit } from '@angular/core';
 import { Confession } from 'src/app/models/confession.model';
 
+// @ts-ignore
 @Component({
   selector: 'app-confession-item',
   templateUrl: './confession-item.component.html',
   styleUrls: ['./confession-item.component.css']
 })
+
 export class ConfessionItemComponent implements OnInit {
   @Input() confession!: Confession;
   constructor() { }
 
   ngOnInit(): void {
-
   }
 
+  addLike(): void {
+    this.confession.likes++;
+  }
 
+  addDislike(): void {
+    this.confession.dislikes++;
+  }
 }
